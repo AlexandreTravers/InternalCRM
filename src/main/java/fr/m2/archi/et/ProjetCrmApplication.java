1dc9c578ca44413a67c5c30933e244edd0901f47
Alexandre  Travers <102867623+AlexandreTravers@users.noreply.github.com>
2023-10-13 09:57:59 +0200

    Initial commit

---
+++ src/main/java/fr/m2/archi/et/ProjetCrmApplication.java
@@ -0,0 +1,13 @@
+package fr.m2.archi.et;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCrmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fr/m2/archi/et/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.m2.archi.et;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjetCrmApplication.class);
+	}
+
+}
