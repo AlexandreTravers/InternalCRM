962f4ffc5e9fd8e049be0d4e0ebc797e18301d74
Alexandre  Travers <102867623+AlexandreTravers@users.noreply.github.com>
2023-11-29 14:00:34 +0100

    Réparation CRM interne (bug fixes)

--- src/main/java/fr/m2/archi/et/service/controller/InternalCRMController.java
+++ src/main/java/fr/m2/archi/et/service/controller/InternalCRMController.java
@@ -2,6 +2,7 @@ package fr.m2.archi.et.service.controller;
 
 import java.util.List;
 
+import org.apache.thrift.TException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,17 +20,17 @@ public class InternalCRMController {
 	@Autowired
     private CRMThriftClient crmClient;
 
-    @GetMapping("/getLeadsWithGet")
+    @GetMapping("/getLeads")
     public List<InternalLeadDto> getLeads(
             @RequestParam double lowAnnualRevenue,
             @RequestParam double highAnnualRevenue,
-            @RequestParam String state) {
+            @RequestParam String state) throws TException {
         return crmClient.findLeads(lowAnnualRevenue, highAnnualRevenue, state);
     }
     
     @PostMapping("/getLeads")
     public List<InternalLeadDto> getLeads(
-    		@RequestBody JsonRequestForLeads request) {
+    		@RequestBody JsonRequestForLeads request) throws TException {
     	return crmClient.findLeads(request.getLowAnnualRevenue(), request.getHighAnnualRevenue(), request.getState());
     }
 }
